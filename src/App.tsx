b67e8b0f7cee54f901930a0d5de8d2ba46654e4b
brayanCh <brayanchavarro081@gmail.com>
2023-08-27 20:55:32 -0500

    begginning react and vite

---
+++ src/App.tsx
@@ -0,0 +1,20 @@
+import { useState } from 'react'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App()
+{
+    const [count, setCount] = useState<number>(0);
+
+    return (
+        <>
+            <div>
+                <a href="https://vitejs.dev" target="_blank">
+                    <img src={viteLogo} className="logo" alt="Vite logo" />
+                </a>
+            </div>
+        </>
+    )
+}
+
+export default App
